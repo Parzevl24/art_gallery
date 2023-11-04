8c31ea17af63068c8426d462bb458e859745c016
Priyansu Kumar <priyansu.kumar2403@gmail.com>
2023-11-04 14:23:43 +0530

    check

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: {type: String},
     password: String,
-    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
   });
 
 const User = mongoose.model('User', userSchema);
